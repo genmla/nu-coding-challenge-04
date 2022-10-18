73f182f69f991953ff03b2b7287e51e35a49c79c
genmla <gmmladenka@gmail.com>
2022-10-17 22:59:55 -0500

    add elements and pseudo code

---
+++ assets/scripts.js
@@ -0,0 +1,24 @@
+//dispaly different divs according to interaction
+
+//start with start div
+
+//hide start div when start button is clicked
+
+//display questions with conditions for quiz div (for loop?) after start button is clicked
+
+//hide quiz div when timer ends or last question answered
+
+//display final div when timer ends or last quesiton answered
+
+//display highscore div when submit button clicked for initials
+
+//render array of scores from highest to lowers
+//log high scores in local storage, push values into highscore array, sort array
+//numberArray.sort(compareNumbers);
+//or append new score above or below depending on value (how would we do this for multiple values?)
+
+//clear high scores when clear highscores button clicked
+
+//display start div when play again button clicked
+
+
