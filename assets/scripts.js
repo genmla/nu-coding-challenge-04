08c17977aabb201ae4c7160919d148a33eee182f
genmla <gmmladenka@gmail.com>
2022-10-24 00:03:00 -0500

    add js to store highscores

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,4 @@
 
-//hide quiz div when timer ends (done) or last question answered (DONE!)
-
-//display final div when timer ends (done) or last quesiton answered (DONE!)
-
 //render array of scores from highest to lowest
 //log high scores in local storage, push values into highscore array, sort array
 //numberArray.sort(compareNumbers);
@@ -20,19 +16,12 @@ var incorrectmsg = document.getElementById("incorrectmsg");
 var nextBtn = document.createElement("button");
 nextBtn.textContent = "Next Question";
 var final = document.getElementById("final");
-var calcBtn = document.getElementById("calcBtn")
-var form = document.getElementById("form")
 var submitBtn = document.getElementById("submitBtn");
 var highscore = document.getElementById("highscore");
-var highscoresList = document.getElementById("highscoresList");
+var highscores = [];
+var previousHighScores = [];
 var againBtn = document.getElementById("againBtn");
 var clearBtn = document.getElementById("clearBtn");
-
-function ShowHighScores() {
-
-    var initial = localStorage.getItem("initial")
-    var finalscore = localStorage.getItem("finalscore")
-}
 
 startBtn.addEventListener("click", function () {
     var timerFunction = setInterval(function () {
@@ -61,10 +50,6 @@ quiz.addEventListener("click", function (event) {
         this.parentElement.append(nextBtn)
         nextBtn.style.display = "block"
         score = score + 10
-
-        var buildscore = score + time
-        console.log(buildscore)
-        localStorage.setItem("buildscore", buildscore)
     }
     else if (answer.matches("li")) {
         response.style.display = "block"
@@ -72,10 +57,6 @@ quiz.addEventListener("click", function (event) {
         this.parentElement.append(nextBtn)
         nextBtn.style.display = "block"
         time = time - 5
-
-        var buildscore = score + time
-        console.log(buildscore)
-        localStorage.setItem("buildscore", buildscore)
     }
 });
 
@@ -89,7 +70,7 @@ nextBtn.addEventListener("click", function () {
     if (quiz.hasChildNodes()) {
         quiz.children[0].setAttribute("style", "display: block;")
     }
-    //why doesnt' this do this: end timer countdown, hides timer, shows final page (IT DOES NOW HAHA, it was an issue with children <0, that's no possible)
+    //why doesn't this do this: end timer countdown, hides timer, shows final page (IT DOES NOW HAHA, it was an issue with children <0, that's no possible)
     else if (quiz.children < 1) {
         clearInterval(timerFunction);
         this.style.display = "none";
@@ -104,28 +85,55 @@ var finish = document.createElement("h2");
 finish.textContent = "Finished!"
 final.children[0].append(finish)
 
-calcBtn.addEventListener("click", function () { 
-this.style.display = "none"
-var finalscore = (score + time)
-var finalscorep = document.createElement("p");
-finalscorep.textContent = "You scored " + finalscore + " this round!";
-final.children[0].append(finalscorep);
-form.style.display = "block"
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault()
 
+    final.style.display = "none";
+    // LogRoundScore();
+    finalscore = (score + time) * 10
+    console.log(finalscore)
+
+    // LogRoundInitials();
+    var initials = document.querySelector("#initials").value;
+    console.log(initials)
+
+    highscore.style.display = "block";
+
+    var finalscorep = document.createElement("p");
+    finalscorep.textContent = "You scored " + finalscore + " this round!";
+    highscore.children[0].append(finalscorep);
+
+    //working
+
+    previousHighScores = JSON.parse(localStorage.getItem("highscores"))
+    console.log(previousHighScores)
+
+    var thisroundLog = {
+        initialslog: initials,
+        finalscorelog: finalscore
+    };
+    localStorage.setItem("thisroundLog", JSON.stringify(thisroundLog))
+    console.log(thisroundLog)
+
+    previousHighScores.push(thisroundLog)   
+    localStorage.setItem("highscores", JSON.stringify(previousHighScores))
+    console.log(highscores)
+
+    var thisroundli = document.createElement("li")
+    thisroundli.textContent = thisroundLog.finalscorelog + " .................... " + thisroundLog.initialslog
+    highscore.children[2].append(thisroundli)
+    //working  
+
+    var initialsli = document.createElement("li");
+    initialsli.textContent = initials;
+    var highli = document.createElement("li");
+    highli.textContent = finalscore;
+    highscore.children[2].append(initialsli)
+    highscore.children[2].append(highli)
 })
 
 //issue: does not display highscore div and it is driving me to rage
 //PREVENT DEFAULT DUDE IT WAS A FOOOOOORM!!!
-submitBtn.addEventListener("click", function (event) {
-    //log initials and score in local storage
-    // localStorage.setItem("highscores", score + time)
-    // localStorage.setItem("initials", score)
-    form.style.display = "none"
-    console.log(score + time)
-    event.preventDefault();
-    final.style.display = "none";
-    highscore.style.display = "block";
-})
 
 againBtn.addEventListener("click", function () {
     location.reload()
